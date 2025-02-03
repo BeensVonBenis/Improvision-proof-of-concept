9bf79d95afc2d239d82cefe7e0a2c415b4d40325
Beens von Benis Kowalikov <145481307+BeensVonBenis@users.noreply.github.com>
2025-02-03 14:07:48 +0100

    Refectoring część 1

---
+++ lib/util/code_orchestra.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:dart_melty_soundfont/synthesizer.dart';
+import 'package:dart_melty_soundfont/synthesizer_settings.dart';
+import 'package:dart_melty_soundfont/array_int16.dart';
+import 'package:dart_melty_soundfont/preset.dart';
+import 'code_orchestra.dart';
+
+class CodeOrchestra {
+  CodeOrchestra() {}
+}
